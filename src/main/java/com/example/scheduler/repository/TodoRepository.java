72be1b36235894de7d37612e639097979cfb1da2
nahyun <nahyun040411@naver.com>
2025-06-10 16:06:23 +0900

    [feat] Repository 메서드 추가

--- src/main/java/com/example/scheduler/repository/TodoRepository.java
+++ src/main/java/com/example/scheduler/repository/TodoRepository.java
@@ -4,9 +4,12 @@ import com.example.scheduler.domain.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     List<Todo> findByUserIdAndDueDate(Long userId, LocalDate dueDate);
     List<Todo> findByUserIdAndIsCompletedFalse(Long userId);
+
+    Collection<Todo> findByUserIdAndIsCompletedFalseAndDueDateBefore(Long userId, LocalDate today);
 }
